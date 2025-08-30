459a43a58cf5440ff3efcc0e34ffc48c121ebcec
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-08-30 08:46:24 +0530

    config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,5 @@
 // export const BASE_URL = "http://localhost:3000";
 
 // Handle both
-// export const BASE_URL =
-// 	location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
-// Handle Vercel
 export const BASE_URL =
-	location.hostname === "localhost"
-		? "http://localhost:3000"
-		: "https://devmate-backend-api.vercel.app";
+	location.hostname === "localhost" ? "http://localhost:3000" : "/api";
